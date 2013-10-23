3d723bba7ffb7ba28a623f15a7070a72bdbda51b
neo <canguan168@gmail.com>
2013-10-23 16:56:04 +0800

    opencart basic
    basic

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/tshirt/admin/');
+define('HTTP_CATALOG', 'http://localhost/tshirt/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/tshirt/admin/');
+define('HTTPS_CATALOG', 'http://localhost/tshirt/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\tshirt/admin/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\tshirt/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\tshirt/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\tshirt/admin/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\tshirt/admin/view/template/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\tshirt/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\tshirt/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\tshirt/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\tshirt/download/');
+define('DIR_LOGS', 'D:\xampp\htdocs\tshirt/system/logs/');
+define('DIR_CATALOG', 'D:\xampp\htdocs\tshirt/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tshirt');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/tshirt/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/tshirt/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\tshirt/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\tshirt/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\tshirt/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\tshirt/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\tshirt/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\tshirt/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\tshirt/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\tshirt/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\tshirt/download/');
+define('DIR_LOGS', 'D:\xampp\htdocs\tshirt/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tshirt');
+define('DB_PREFIX', 'oc_');
+?>
